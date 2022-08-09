16944d4dcb63c87e47a21b4a4db2cec8c0c4469f
emilylubkert <93680915+emilylubkert@users.noreply.github.com>
2022-08-08 22:21:21 -0600

    update file structure

--- test/test_battery/test_nubbin.py
+++ test/test_battery/test_nubbin.py
@@ -2,7 +2,6 @@ import unittest
 from datetime import datetime
 
 from battery.nubbin_battery import NubbinBattery
-from battery.spindler_battery import SpindlerBattery
 
 class TestNubbinBattery(unittest.TestCase):
     def test_battery_should_be_serviced(self):
@@ -14,16 +13,4 @@ class TestNubbinBattery(unittest.TestCase):
         today = datetime.today().date()
         last_service_date = today.replace(year=today.year - 1)
         battery = NubbinBattery(last_service_date, today)
-        self.assertFalse(battery.needs_service())
-
-class TestSpindlerBattery(unittest.TestCase):
-    def test_battery_should_be_serviced(self):
-        today = datetime.today().date()
-        last_service_date = today.replace(year=today.year - 3)
-        battery = SpindlerBattery(last_service_date, today)
-        self.assertTrue(battery.needs_service())
-    def test_battery_should_not_be_serviced(self):
-        today = datetime.today().date()
-        last_service_date = today.replace(year=today.year - 1)
-        battery = SpindlerBattery(last_service_date, today)
         self.assertFalse(battery.needs_service())
---
+++ test/test_engine/test_willoughby.py
@@ -0,0 +1,16 @@
+import unittest
+from datetime import datetime
+
+from engine.willoughby_engine import WilloughbyEngine
+
+class TestWilloughbyEngine(unittest.TestCase):
+    def engine_should_be_serviced(self):
+        current_mileage = 100000
+        last_service_mileage = 20000
+        engine = WilloughbyEngine(last_service_mileage, current_mileage)
+        self.assertTrue(engine.needs_service())
+    def engine_should_not_be_serviced(self):
+        current_mileage = 40000
+        last_service_mileage = 20000
+        engine = WilloughbyEngine(last_service_mileage, current_mileage)
+        self.assertFalse(engine.needs_service())
